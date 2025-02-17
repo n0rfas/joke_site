0bf994225ba0025ad799066ff957a90b9011baec
n0rfas <antsa@yandex.ru>
2025-02-17 20:44:48 +0100

    infrastructure refactoring

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -2,12 +2,11 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
+from app.models import Base
+from app.settings import Settings
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from app.models import Base
-from app.settings import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
